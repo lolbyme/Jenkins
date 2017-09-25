59fe19d233228d167d91fdeff3686da1c319c8ed
lolbyme <32265064+lolbyme@users.noreply.github.com>
2017-09-25 14:27:34 +0200

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+package ca.uwo.csd.cs2212.Zsolt;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestBankAccount {
+
+ @Test
+ public void testDebitWithSufficientFunds(){
+  Assert.assertEquals(5.0,4.0);
+ }
+}
